2ae76e0f918726f2a91b7579179e40166025c227
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-23 07:39:44 +0000

    changes counter text buttons to use svgs

---
+++ src/app/components/reducer/index.ts
@@ -0,0 +1,11 @@
+
+// // import { AppState } from "../counter/selectors/counter.selector"
+// import { counterActions } from "../counter/actions/counter.action"
+// import { _counterReducer } from "../counter/reducers/counter.reducer"
+// import { counterType } from "../counter/reducers/counter.reducer"
+
+
+// export function reducers(state:counterType | undefined, action:counterActions): counterType {
+//     return _counterReducer(state, action)
+//     // return counterReducer(state ?? 0, action as counterActions)
+// }
